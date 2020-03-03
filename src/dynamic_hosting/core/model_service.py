ac0f8ae2d9d87c2ba110da98109d0ef792096247
ke li <Ke.Li1@ibm.com>
2020-03-03 10:01:58 +0100

    Add model addition/deletion functionality in model_service

--- src/dynamic_hosting/core/model_service.py
+++ src/dynamic_hosting/core/model_service.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 import logging
-
-from typing import Mapping, Text, Optional, Sequence, Any, MutableMapping
 from pathlib import Path
+from typing import Mapping, Text, Any
 
 from pandas import DataFrame
 
@@ -10,12 +9,22 @@ from .model import MLModel
 
 
 class ModelService:
-    def __init__(self, ml_models: Mapping[Text, Mapping[Text, MLModel]]):
+    def __init__(
+            self,
+            ml_models: Mapping[Text, Mapping[Text, MLModel]],
+            storage_root: Path
+    ):
         self.ml_models: Mapping[Text, Mapping[Text, MLModel]] = ml_models
+        self.storage_root: Path = storage_root
 
-    def invoke(self, model_name: Text, model_version: Text, data: DataFrame) -> Any:
+    def invoke(
+            self,
+            model_name: Text,
+            model_version: Text,
+            data: DataFrame
+    ) -> Any:
         logger = logging.getLogger(__name__)
-        logger.info('Invoke ml model <{name}> version <{version}>'.format(name=model_name, version=model_version))
+        logger.debug('Invoke ml model <{name}> version <{version}>'.format(name=model_name, version=model_version))
 
         if not self.ml_models.get(model_name):
             raise RuntimeError('Model <{name}> not found'.format(name=model_name))
@@ -27,12 +36,44 @@ class ModelService:
 
         return self.ml_models[model_name][model_version].invoke(data_input=data)
 
+    def add_model(
+            self, model: MLModel
+    ) -> None:
+        model.save_to_disk(self.storage_root)
+        self.reload_models()
+
+    def remove_model(
+            self,
+            model_name: Text,
+            model_version: Text = None
+    ) -> None:
+        MLModel.remove_from_disk(
+            storage_root=self.storage_root,
+            model_name=model_name,
+            model_version=model_version
+        )
+        self.reload_models()
+
+    def reload_models(self) -> None:
+        self.ml_models = ModelService.__load_models_from_disk(self.storage_root)
+
     @staticmethod
-    def load_from_disk(storage_root: Path) -> 'ModelService':
+    def load_from_disk(
+            storage_root: Path
+    ) -> 'ModelService':
         logger = logging.getLogger(__name__)
         logger.info('Loading ML service from storage root: {storage_root}'.format(storage_root=storage_root))
 
-        ml_models: Mapping[Text, Mapping[Text, MLModel]] = {
+        return ModelService(
+            ml_models=ModelService.__load_models_from_disk(storage_root=storage_root),
+            storage_root=storage_root
+        )
+
+    @staticmethod
+    def __load_models_from_disk(
+            storage_root: Path
+    ) -> Mapping[Text, Mapping[Text, MLModel]]:
+        return {
             model_abspath.name:
                 {
                     versioned_model_abspath.name:
@@ -46,5 +87,3 @@ class ModelService:
             for versioned_model_abspath in model_abspath.iterdir()
             if versioned_model_abspath.is_dir()
         }
-
-        return ModelService(ml_models)
