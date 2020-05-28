6ff0b61de57ab56158c1de8f636cb62a04fdba14
ke li <Ke.Li1@ibm.com>
2020-05-28 17:28:57 +0200

    Add log for deployment script

--- scripts/ibm_image_registry.sh
+++ scripts/ibm_image_registry.sh
@@ -7,32 +7,43 @@ set -o nounset
 set -o pipefail
 
 
+YELLOW='\033[1;33m'
+NC='\033[0m'
+APP_HOME="/app"
+
+
 IMAGE_TAG="${DEPLOY_TIMESTAMP}-${TRAVIS_BUILD_NUMBER}-${TRAVIS_BRANCH//[\/]/-}"
 
 
 install_ibm_cloud_cli() {
+  echo "${YELLOW}[INFO] Installing CLI tools${NC}"
   curl -sL https://ibm.biz/idt-installer | bash
 }
 
 ibm_cloud_login() {
+  echo "${YELLOW}[INFO] Authenticating ibm cloud${NC}"
   ibmcloud login -a https://api.ng.bluemix.net --apikey "${IBMCLOUD_API_KEY}"
 }
 
 ibm_container_registry_login() {
+  echo "${YELLOW}[INFO] Authenticating ibm container registry${NC}"
   ibmcloud cr login
 }
 
 tag_image() {
+  echo "${YELLOW}[INFO] Tagging image${NC}"
   docker tag open-prediction:"${IMAGE_TAG}" us.icr.io/"${CR_NAMESPACE}"/open-prediction:"${IMAGE_TAG}"
   docker tag open-prediction:latest us.icr.io/"${CR_NAMESPACE}"/open-prediction:latest
 }
 
 push_image() {
+  echo "${YELLOW}[INFO] Pushing image${NC}"
   docker push us.icr.io/"${CR_NAMESPACE}"/open-prediction:"${IMAGE_TAG}"
   docker push us.icr.io/"${CR_NAMESPACE}"/open-prediction:latest
 }
 
 deploy() {
+  echo "${YELLOW}[INFO] Begin deployment${NC}"
   install_ibm_cloud_cli
   ibm_cloud_login
   ibm_container_registry_login
