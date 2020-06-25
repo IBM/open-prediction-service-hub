66e4541e846b2e566db4d7a0c78e2a04cf47fe27
ke li <Ke.Li1@ibm.com>
2020-06-25 15:08:48 +0200

    Single-sourcing the package version

--- ml-service-implementations/ads-ml-service/setup.py
+++ ml-service-implementations/ads-ml-service/setup.py
@@ -1,12 +1,31 @@
 from pathlib import Path
+from typing import Text
+
 from setuptools import setup, find_packages
 
+import re
 
 PROJECT_ROOT = Path(__file__).resolve().parent
 with PROJECT_ROOT.joinpath('requirements.txt').open(mode='r', encoding='utf-8') as fd:
     installation_dependencies = [i.strip() for i in fd.readlines()]
 
+
+def __get_version(init_file: Text) -> Text:
+    here: Path = Path(__file__).resolve().parent
+    with here.joinpath(init_file).open(mode='r') as fp:
+        contents: Text = fp.read()
+
+    for line in contents.splitlines():
+        if line.startswith('__version__'):
+            result = re.search(pattern=r'([\d.]+).*', string=line)
+            if result is not None:
+                return result.group(1)
+            else:
+                raise RuntimeError('Unable to find version string.')
+
+
 setup(
+    version=__get_version('src/predictions/__init__.py'),
     zip_safe=True,
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
--- ml-service-implementations/ads-ml-service/src/predictions/main.py
+++ ml-service-implementations/ads-ml-service/src/predictions/main.py
@@ -20,10 +20,11 @@ from typing import Text
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
+from . import __version__
 from .api.api_v1.api import api_router
 
 app: FastAPI = FastAPI(
-    version='0.0.1',
+    version=__version__,
     title='Open Prediction Service',
     description='A simple Machine Learning serving environment for tests',
     openapi_url="/open-prediction-service.json"
