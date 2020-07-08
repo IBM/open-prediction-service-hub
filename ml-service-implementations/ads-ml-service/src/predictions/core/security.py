97f5c78d811050be6bd3360a0290b6fbb9fdddd7
ke li <Ke.Li1@ibm.com>
2020-07-08 10:49:12 +0200

    [RTC-130409] Add token generation utilities

--- ml-service-implementations/ads-ml-service/src/predictions/core/security.py
+++ ml-service-implementations/ads-ml-service/src/predictions/core/security.py
@@ -15,4 +15,35 @@
 #
 
 
+from datetime import datetime, timedelta
+from typing import Union, Text, Any
+
+import jwt
+from passlib.context import CryptContext
+
+from ..core.configuration import get_config
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
 ALGORITHM = 'HS256'
+
+
+def create_access_token(
+    subject: Union[Text, Any], expires_delta: timedelta = None
+) -> Text:
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=get_config().ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode = {'sub': str(subject), 'exp': expire}
+    encoded_jwt = jwt.encode(to_encode, get_config().SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_pwd(plain_password: Text, hashed_password: Text) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_pwd_hash(password: Text) -> Text:
+    return pwd_context.hash(password)
