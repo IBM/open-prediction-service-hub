2f30ff8df409af67f678347a9a14a74697b227a2
ke li <Ke.Li1@ibm.com>
2020-07-21 14:20:21 +0200

    [RTC-130409] Add default certificates

--- ml-service-implementations/ads-ml-service/entrypoint.sh
+++ ml-service-implementations/ads-ml-service/entrypoint.sh
@@ -8,6 +8,21 @@ set -e
 # shellcheck disable=SC1090
 . "${OPS_HOME}"/prestart.sh
 
+
+if [ ! -f "${MODEL_STORAGE}/key.pem" ] || [ ! -f "${MODEL_STORAGE}/cert.pem" ]; then
+  # default certificate
+  echo "[INFO] SSL certificates not found"
+  echo "[INFO] Creating default certificates"
+  openssl req \
+    -x509 -newkey rsa:4096 -sha512 \
+    -nodes \
+    -subj '/CN=localhost' \
+    -days 365 \
+    -keyout "${MODEL_STORAGE}/key.pem" -out "${MODEL_STORAGE}/cert.pem"
+fi
+
+
+
 # Default parameters:
 #   Uvicorn worker class is required by Fastapi
 #   Container schedulers typically expect logs to come out on stdout/stderr, thus gunicorn is configured to do so
@@ -19,4 +34,6 @@ exec gunicorn \
             --worker-tmp-dir=/dev/shm \
             --config=file:gunicorn.init.py \
             --bind=:8080 \
+            --certfile="${MODEL_STORAGE}/cert.pem" \
+            --keyfile="${MODEL_STORAGE}/key.pem" \
             main:app
