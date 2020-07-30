37b885aea4b6d9594242e25c3fc550328df3a5a1
ke li <Ke.Li1@ibm.com>
2020-07-30 18:57:27 +0200

    [RTC-131299] Make the deploy script more generic

--- ml-service-implementations/ads-ml-service/kubernetes/docker_image_registry.sh
+++ ml-service-implementations/ads-ml-service/kubernetes/docker_image_registry.sh
@@ -10,21 +10,26 @@ set -o pipefail
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-IMAGE_NAME="decisions-tooling/ads-ml-service"
-IMAGE_TAG="${RELEASE_YEAR}${DESIGNER_COMPATIBLE_REL}"
-IMAGE="${IMAGE_NAME}:${IMAGE_TAG}"
+# Required EN vars:
+#   REGISTRY_URL
+#   DOCKER_NAMESPACE
+#   FULL_TAG: offered by .travis
+DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-"ads-ml-service"}
+DOCKER_TAG=${DOCKER_TAG:-"${RELEASE_YEAR}${DESIGNER_COMPATIBLE_REL}"}
 
 login() {
   echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin "${REGISTRY_URL}"
 }
 
 tag_image() {
-  docker tag ads-ml-service:latest "${REGISTRY_URL}/${IMAGE}"
+  docker tag ads-ml-service:latest "${REGISTRY_URL}/${DOCKER_NAMESPACE}/${DOCKER_REPOSITORY}:${DOCKER_TAG}"
+  docker tag ads-ml-service:latest "${REGISTRY_URL}/${DOCKER_NAMESPACE}/${DOCKER_REPOSITORY}:${FULL_TAG}"
 }
 
 push_image() {
-  echo -e "${YELLOW}[INFO] Pushing image ${REGISTRY_URL}/${IMAGE}${NC}"
-  docker push "${REGISTRY_URL}/${IMAGE}"
+  echo -e "${YELLOW}[INFO] Pushing image for repo: ${REGISTRY_URL}/${DOCKER_NAMESPACE}/${DOCKER_REPOSITORY}${NC}"
+  docker push "${REGISTRY_URL}/${DOCKER_NAMESPACE}/${DOCKER_REPOSITORY}:${DOCKER_TAG}"
+  docker push "${REGISTRY_URL}/${DOCKER_NAMESPACE}/${DOCKER_REPOSITORY}:${FULL_TAG}"
 }
 
 
