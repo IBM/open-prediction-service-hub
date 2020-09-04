fcc00c9a5829011b7c992b71b7dd035225e9074f
ke li <Ke.Li1@ibm.com>
2020-09-04 09:14:54 +0200

    Add testes for cascade delete

--- ml-service-implementations/ads-ml-service/app/tests/crud/test_endpoint.py
+++ ml-service-implementations/ads-ml-service/app/tests/crud/test_endpoint.py
@@ -15,22 +15,38 @@
 #
 
 
+import pickle
 import typing
 
 import sqlalchemy.orm as orm
 
+import app.core.supported_lib as supported_lib
 import app.crud as crud
 import app.models as models
 import app.schemas as schemas
 import app.tests.utils.utils as utils
 
 
-def test_create_endpoint(
-        db: orm.Session, model_in_db: models.Model
-) -> typing.NoReturn:
+def test_create_endpoint(db: orm.Session, model_in_db: models.Model) -> typing.NoReturn:
     endpoint_name = utils.random_string()
     endpoint_in = schemas.EndpointCreate(name=endpoint_name)
     endpoint = crud.endpoint.create_with_model(db, obj_in=endpoint_in, model_id=model_in_db.id)
 
     assert endpoint.name == endpoint_name
     assert endpoint.model_id == model_in_db.id
+
+
+def test_cascade_delete_with_binary(
+        db: orm.Session,
+        endpoint_in_db: models.Model,
+        classification_predictor: object
+):
+    binary_in = schemas.BinaryMlModelCreate(
+        model_b64=pickle.dumps(classification_predictor),
+        library=supported_lib.MlLib.SKLearn
+    )
+    binary = crud.binary_ml_model.create_with_endpoint(db, obj_in=binary_in, endpoint_id=endpoint_in_db.id)
+    crud.endpoint.delete(db, id=endpoint_in_db.id)
+    binary_1 = crud.binary_ml_model.get(db, id=binary.id)
+
+    assert binary_1 is None
