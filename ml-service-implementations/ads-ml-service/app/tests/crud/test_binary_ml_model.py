82a5f7df6b8a37bdfdbf7aa7cd2c18672442fc80
ke li <Ke.Li1@ibm.com>
2020-09-04 09:40:10 +0200

    Refactor testes for binary crud

--- ml-service-implementations/ads-ml-service/app/tests/crud/test_binary_ml_model.py
+++ ml-service-implementations/ads-ml-service/app/tests/crud/test_binary_ml_model.py
@@ -20,74 +20,62 @@ import typing
 
 import sqlalchemy.orm as orm
 
-import app.core.supported_lib as supported_lib
 import app.crud as crud
-import app.schemas as schemas
 import app.models as models
+import app.schemas as schemas
 
 
 def test_create_binary_ml_model(
-    db: orm.Session,
-    endpoint_in_db: models.Endpoint,
-    classification_predictor: object
+        db: orm.Session,
+        endpoint_in_db: models.Endpoint,
+        binary_create: schemas.BinaryMlModelCreate
 ) -> typing.NoReturn:
-    binary_in = schemas.BinaryMlModelCreate(
-        model_b64=pickle.dumps(classification_predictor),
-        library=supported_lib.MlLib.SKLearn
-    )
-    binary = crud.binary_ml_model.create_with_endpoint(db, obj_in=binary_in, endpoint_id=endpoint_in_db.id)
+    binary = crud.binary_ml_model.create_with_endpoint(db, obj_in=binary_create, endpoint_id=endpoint_in_db.id)
 
     assert binary.endpoint_id == endpoint_in_db.id
-    assert type(pickle.loads(binary.model_b64)) == type(classification_predictor)
-    assert binary.library == supported_lib.MlLib.SKLearn
+    assert isinstance(pickle.loads(binary.model_b64), type(pickle.loads(binary_create.model_b64)))
+    assert binary.library == binary_create.library
 
 
 def test_get_binary_ml_model(
-    db: orm.Session,
-    endpoint_in_db: models.Endpoint,
-    classification_predictor: object
+        db: orm.Session,
+        endpoint_in_db: models.Endpoint,
+        binary_create: schemas.BinaryMlModelCreate
 ) -> typing.NoReturn:
-    binary_in = schemas.BinaryMlModelCreate(
-        model_b64=pickle.dumps(classification_predictor),
-        library=supported_lib.MlLib.SKLearn
-    )
-    binary = crud.binary_ml_model.create_with_endpoint(db, obj_in=binary_in, endpoint_id=endpoint_in_db.id)
+    binary = crud.binary_ml_model.create_with_endpoint(db, obj_in=binary_create, endpoint_id=endpoint_in_db.id)
     binary_1 = crud.binary_ml_model.get(db, id=binary.id)
 
-    assert type(pickle.loads(binary_1.model_b64)) == type(classification_predictor)
-    assert binary_1.library == binary.library
+    assert binary_1.id == binary.id
+    assert binary_1.endpoint_id == endpoint_in_db.id
+    assert isinstance(pickle.loads(binary_1.model_b64), type(pickle.loads(binary_create.model_b64)))
+    assert binary_1.library == binary_create.library
 
 
 def test_get_binary_ml_model_by_endpoint(
-    db: orm.Session,
-    endpoint_in_db: models.Endpoint,
-    classification_predictor: object
+        db: orm.Session,
+        endpoint_in_db: models.Endpoint,
+        binary_create: schemas.BinaryMlModelCreate
 ) -> typing.NoReturn:
-    binary_in = schemas.BinaryMlModelCreate(
-        model_b64=pickle.dumps(classification_predictor),
-        library=supported_lib.MlLib.SKLearn
-    )
-    binary = crud.binary_ml_model.create_with_endpoint(db, obj_in=binary_in, endpoint_id=endpoint_in_db.id)
+    binary = crud.binary_ml_model.create_with_endpoint(db, obj_in=binary_create, endpoint_id=endpoint_in_db.id)
     binary_1 = crud.binary_ml_model.get_by_endpoint(db, endpoint_id=endpoint_in_db.id)
 
-    assert type(pickle.loads(binary_1.model_b64)) == type(classification_predictor)
-    assert binary_1.library == binary.library
+    assert binary_1.id == binary.id
+    assert binary_1.endpoint_id == endpoint_in_db.id
+    assert isinstance(pickle.loads(binary_1.model_b64), type(pickle.loads(binary_create.model_b64)))
+    assert binary_1.library == binary_create.library
 
 
 def test_delete_binary_ml_model(
-    db: orm.Session,
-    endpoint_in_db: models.Endpoint,
-    classification_predictor: object
+        db: orm.Session,
+        endpoint_in_db: models.Endpoint,
+        binary_create: schemas.BinaryMlModelCreate
 ) -> typing.NoReturn:
-    binary_in = schemas.BinaryMlModelCreate(
-        model_b64=pickle.dumps(classification_predictor),
-        library=supported_lib.MlLib.SKLearn
-    )
-    binary = crud.binary_ml_model.create_with_endpoint(db, obj_in=binary_in, endpoint_id=endpoint_in_db.id)
+    binary = crud.binary_ml_model.create_with_endpoint(db, obj_in=binary_create, endpoint_id=endpoint_in_db.id)
     binary_1 = crud.binary_ml_model.delete(db, id=binary.id)
     binary_2 = crud.binary_ml_model.get(db, id=binary.id)
 
     assert binary_2 is None
     assert binary_1.id == binary.id
-    assert type(pickle.loads(binary_1.model_b64)) == type(classification_predictor)
-    assert binary_1.library == binary.library
+    assert binary_1.endpoint_id == endpoint_in_db.id
+    assert isinstance(pickle.loads(binary_1.model_b64), type(pickle.loads(binary_create.model_b64)))
+    assert binary_1.library == binary_create.library
