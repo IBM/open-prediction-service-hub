29da1b4d7b7d4dd9b899c2cb357bfb1d7d4e4ad2
ke li <Ke.Li1@ibm.com>
2020-09-07 16:34:02 +0200

    Add /v2/models

---
+++ ml-service-implementations/ads-ml-service/app/api/api_v2/endpoints/models.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020 IBM
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.IBM Confidential
+#
+
+
+import typing
+
+import fastapi
+import fastapi.encoders as encoders
+import fastapi.responses as responses
+import sqlalchemy.orm as saorm
+import starlette.status as status
+
+import app.api.deps as deps
+import app.crud as crud
+import app.schemas as schemas
+import app.schemas.impl as impl
+
+router = fastapi.APIRouter()
+
+
+@router.get(
+    path='/models/{model_id}',
+    response_model=impl.ModelImpl
+)
+def get_ml_models_configs(
+        model_id: int,
+        db: saorm.Session = fastapi.Depends(deps.get_db)
+) -> typing.Dict[typing.Text, typing.Any]:
+    return impl.ModelImpl.from_database(
+        db_obj=crud.model.get(db, id=model_id)
+    )
+
+
+@router.get(
+    path='/models',
+    response_model=impl.ModelsImpl
+)
+def get_ml_models_configs(
+        db: saorm.Session = fastapi.Depends(deps.get_db)
+) -> typing.Dict[typing.Text, typing.Any]:
+    return {
+        'models': [
+            impl.ModelImpl.from_database(model)
+            for model in crud.model.get_all(db)
+        ]
+    }
+
+
+@router.post(
+    path='/models',
+    response_model=impl.ModelImpl
+)
+def add_model(
+        m_in: impl.ModelCreateImpl,
+        db: saorm.Session = fastapi.Depends(deps.get_db)
+) -> typing.Dict[typing.Text, typing.Any]:
+    model = crud.model.create(db, obj_in=schemas.ModelCreate(name=m_in.name))
+    crud.model_config.create_with_model(
+        db,
+        obj_in=schemas.ModelConfigCreate(
+            configuration=encoders.jsonable_encoder(obj=m_in)
+        ),
+        model_id=model.id
+    )
+    return impl.ModelImpl.from_database(
+        db_obj=model
+    )
+
+
+@router.patch(
+    path='/models/{model_id}',
+    response_model=impl.ModelImpl
+)
+def patch_model(
+        model_id: int,
+        m_in: impl.ModelUpdateImpl,
+        db: saorm.Session = fastapi.Depends(deps.get_db)
+) -> typing.Dict[typing.Text, typing.Any]:
+    update_data = m_in.dict(exclude_unset=True)
+    model = crud.model.get(db, id=model_id)
+    new_name = m_in.name if m_in.name else model.name
+    new_config = {
+        field: update_data[field] if field in update_data else model.config.configuration[field]
+        for field in model.config.configuration
+    }
+    crud.model.update(db, db_obj=model, obj_in=schemas.ModelUpdate(name=new_name))
+    crud.model_config.update(
+        db,
+        db_obj=model.config,
+        obj_in=schemas.ModelConfigUpdate(configuration=new_config)
+    )
+    return impl.ModelImpl.from_database(
+        db_obj=model
+    )
+
+
+@router.delete(
+    path='/models/{model_id}',
+    status_code=status.HTTP_204_NO_CONTENT
+)
+def get_ml_models_configs(
+        model_id: int,
+        db: saorm.Session = fastapi.Depends(deps.get_db)
+) -> typing.NoReturn:
+    model = crud.model.get(db, id=model_id)
+    if not model:
+        return responses.Response(status_code=status.HTTP_204_NO_CONTENT)
+    crud.model.delete(db, id=model_id)
+    return responses.Response(status_code=status.HTTP_204_NO_CONTENT)
