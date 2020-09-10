d1d9348efca109680cfbaa7cde87063de08cb2df
ke li <Ke.Li1@ibm.com>
2020-09-10 18:50:54 +0200

    Update testes for init_db

--- ml-service-implementations/ads-ml-service/app/tests/db/test_init_db.py
+++ ml-service-implementations/ads-ml-service/app/tests/db/test_init_db.py
@@ -16,18 +16,42 @@
 
 
 import typing
+import pathlib
+import json
 
 import sqlalchemy.orm as orm
+import pytest
 
 import app.crud as crud
 
+PROJECT_ROOT = pathlib.Path(__file__).resolve().parents[3].resolve()
+EXAMPLES_ROOT = PROJECT_ROOT.joinpath('examples', 'model_training_and_deployment')
 
+
+@pytest.mark.parametrize(
+    'project_dir',
+    [
+        EXAMPLES_ROOT.joinpath('classification', 'iris_svc'),
+        EXAMPLES_ROOT.joinpath('classification', 'miniloan_linear_svc'),
+        EXAMPLES_ROOT.joinpath('classification', 'miniloan_xgb')
+    ]
+)
 def test_load_models(
     db: orm.Session,
-) -> typing.NoReturn:
+    project_dir: pathlib.Path
+) -> typing.Any:
     import app.db.init_db as init_db
-    project_dir = init_db.EXAMPLES_ROOT.joinpath('classification', 'miniloan_linear_svc')
-    init_db.load_models(db=db, project_dirs=[project_dir])
-    model = crud.model.get_all(db)[0]
+    loaded = init_db.load_models(db=db, project_dirs=[project_dir])
+    with loaded[0][2].open(mode='r') as fd:
+        config = json.load(fd)
+    model = crud.model.get_by_name(db, name=config['model']['name'])
 
     assert model is not None
+    assert model.config is not None
+    assert all(
+        [
+            model.config.configuration[key] == config['model'][key] for key in config['model'].keys()
+        ]
+    )
+    assert model.endpoint[0].name == config['endpoint']['name']
+    assert model.endpoint[0].binary is not None
