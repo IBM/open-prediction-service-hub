17b6aa6b3b48e9b2cd124592c86575c149cf4483
ke li <Ke.Li1@ibm.com>
2020-09-14 09:58:10 +0200

    Refine relative url invocation

--- ml-service-implementations/ads-ml-service/app/core/uri.py
+++ ml-service-implementations/ads-ml-service/app/core/uri.py
@@ -19,5 +19,5 @@ import re
 
 # TEMPLATE = 'ops:///{resource_type}/{resource_id}'  #  OPS specific uri scheme is not used
 TEMPLATE = '/{resource_type}/{resource_id}'
-RE_TEMPLATE = r'^(?P<scheme>[a-zA-Z0-9+.-]+://)[^/]*(?P<resource_path>.*)/(?P<resource_id>.*)$'
+RE_TEMPLATE = r'^(?P<scheme>[a-zA-Z0-9+.-]+)?(?:\://)?[^/]*(?P<resource_path>.*)/(?P<resource_id>.*)$'
 ADS_ML_SERVICE_RE = re.compile(RE_TEMPLATE)
