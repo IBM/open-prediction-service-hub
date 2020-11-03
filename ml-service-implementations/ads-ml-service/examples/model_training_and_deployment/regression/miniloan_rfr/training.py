cac2099b44083a2331338478bdd01bc6621318f6
ke li <Ke.Li1@ibm.com>
2020-11-03 14:57:30 +0100

    Re-Add HPO code in comment section to improve user experience

--- ml-service-implementations/ads-ml-service/examples/model_training_and_deployment/regression/miniloan_rfr/training.py
+++ ml-service-implementations/ads-ml-service/examples/model_training_and_deployment/regression/miniloan_rfr/training.py
@@ -18,22 +18,19 @@
 import logging
 import pickle
 import sys
-from pathlib import Path
+import pathlib
 
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
+import sklearn.model_selection as model_selection
+import sklearn.ensemble as skl_ensemble
 
 
 def main():
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    logger = logging.getLogger(__name__)
-
     names = ['name', 'creditScore', 'income', 'loanAmount', 'monthDuration', 'approval', 'rate', 'yearlyReimbursement']
     used_names = ['creditScore', 'income', 'loanAmount', 'monthDuration', 'rate', 'yearlyReimbursement']
 
-    miniloan_file = Path(__file__).resolve().parents[3].joinpath('data').joinpath('miniloan-decisions-ls-10K.csv')
+    miniloan_file = pathlib.Path(__file__).resolve().parents[3].joinpath('data').joinpath('miniloan-decisions-ls-10K.csv')
 
     data = pd.read_csv(
         miniloan_file,
@@ -55,28 +52,51 @@ def main():
         [np.inf, -np.inf], np.nan
     ).dropna().loc[:, used_names]
 
-    train, test = train_test_split(data, random_state=7)
+    train, test = model_selection.train_test_split(data, random_state=7)
 
-    logger.debug(f'training size: {len(train)}')
-    logger.debug(f'validation size: {len(test)}')
+    logger.info(f'Training size: {len(train)}')
+    logger.info(f'Validation size: {len(test)}')
 
     x_train = train.loc[:, used_names[:-1]]
     y_train = train.loc[:, used_names[-1]]
 
-    estimator = RandomForestRegressor(
+    # parameter_estimator = model_selection.RandomizedSearchCV(
+    #     **{
+    #         'estimator': skl_ensemble.RandomForestRegressor(random_state=42),
+    #         'cv': 3,
+    #         'verbose': 1,
+    #         'n_jobs': -1,
+    #         'error_score': 'raise',
+    #         'random_state': 7,
+    #         'n_iter': 1000,
+    #         'param_distributions': {
+    #             'criterion': ['mse'],
+    #             'n_estimators': [int(x) for x in np.linspace(50, 1000, num=20)],
+    #             'min_samples_split': [int(x) for x in np.linspace(2, 64, num=50)],
+    #             'min_samples_leaf': [int(x) for x in np.linspace(1, 32, num=20)]
+    #         }
+    #     }
+    # )
+    # parameter_estimator.fit(x_train, y_train)
+    # logger.info(f'Best parameters: {parameter_estimator.best_params_}')
+    # estimator = parameter_estimator.best_estimator_
+
+    # Uncomment lines above to activate HPO
+    estimator = skl_ensemble.RandomForestRegressor(
         random_state=42,
-        n_estimators=500,
-        min_samples_split=8,
-        min_samples_leaf=5
+        n_estimators=900,
+        min_samples_split=33,
+        min_samples_leaf=7,
+        criterion='mse'
     )
 
     estimator.fit(x_train, y_train)
 
     score = estimator.score(test.loc[:, used_names[:-1]],
                             test.loc[:, used_names[-1]])
-    logger.debug(f'coefficient of determination R^2: {score}')
+    logger.info(f'Coefficient of determination R^2: {score}')
 
-    with Path(__file__).resolve().parent.joinpath('model.pkl').open(mode='wb') as fd:
+    with pathlib.Path(__file__).resolve().parent.joinpath('model.pkl').open(mode='wb') as fd:
         pickle.dump(
             obj=estimator,
             file=fd
@@ -84,4 +104,6 @@ def main():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logger = logging.getLogger(__name__)
     main()
