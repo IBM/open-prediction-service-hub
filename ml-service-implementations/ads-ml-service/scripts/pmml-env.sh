a7741d96a9fad55ebe58e5771db3f7d4f5e0971d
ke li <Ke.Li1@ibm.com>
2020-11-17 15:08:47 +0100

    Add dependencies for PMML support

---
+++ ml-service-implementations/ads-ml-service/scripts/pmml-env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+install_jre() {
+  apt-get -qq update
+  mkdir -p /usr/share/man/man1
+  apt-get -qq install --assume-yes default-jre-headless
+  rm -rf /var/lib/apt/lists/*
+}
+
+install_PyPMML() {
+  python3 -m pip install --no-cache-dir --use-feature=2020-resolver pypmml~=0.9.7
+}
+
+install_jre
+install_PyPMML
