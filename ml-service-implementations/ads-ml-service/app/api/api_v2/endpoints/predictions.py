855ccb92528822e9f0f3ddbeae55996a5d22c15a
ke li <Ke.Li1@ibm.com>
2020-11-23 18:14:30 +0100

    Update /predictions for new multi-enum support

--- ml-service-implementations/ads-ml-service/app/api/api_v2/endpoints/predictions.py
+++ ml-service-implementations/ads-ml-service/app/api/api_v2/endpoints/predictions.py
@@ -21,13 +21,11 @@ import typing
 import fastapi
 import sqlalchemy.orm as saorm
 import starlette.status as status
-import pandas as pd
 
 import app.api.deps as deps
-import app.core.cache as ops_cache
-import app.core.uri as ops_rui
-import app.crud as crud
+import app.core.uri as ops_uri
 import app.gen.schemas.ops_schemas as ops_schemas
+import app.runtime.cache as app_cache
 import app.schemas.impl as impl
 
 router = fastapi.APIRouter()
@@ -38,18 +36,19 @@ LOGGER = logging.getLogger(__name__)
     path='/predictions',
     response_model=ops_schemas.PredictionResponse
 )
-def predict(
+async def predict(
         pre_in: impl.PredictionImpl,
         db: saorm.Session = fastapi.Depends(deps.get_db)
 ) -> typing.Dict[typing.Text, typing.Any]:
     LOGGER.info('Prediction input: %s', pre_in)
 
-    endpoint_resource_list = list(filter(lambda link: link.rel == 'endpoint', pre_in.target))
-    if len(endpoint_resource_list) != 1:
+    endpoint_resources = (link for link in pre_in.target if link.rel == 'endpoint')
+    endpoint_resource = next(endpoint_resources, None)
+    if endpoint_resource is None or next(endpoint_resources, None) is not None:
         raise fastapi.HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail='Predict accept one and only one endpoint')
 
-    match = ops_rui.ADS_ML_SERVICE_RE.match(endpoint_resource_list[0].href)
+    match = ops_uri.ADS_ML_SERVICE_RE.match(endpoint_resource.href)
     try:
         resource_path = match.group('resource_path')
         resource_id = int(match.group('resource_id'))
@@ -61,26 +60,17 @@ def predict(
         raise fastapi.HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail='/predictions only accept /endpoints as target')
 
-    endpoint = crud.endpoint.get(db, id=resource_id)
-    if endpoint is None:
-        raise fastapi.HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='/endpoint resource not found')
-
-    deserialized = ops_cache.cache.get_deserialized_model(db, endpoint_id=endpoint.id)
+    deserialized = app_cache.cache.get_deserialized_model(db, endpoint_id=resource_id)
     if not deserialized:
         raise fastapi.HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='Model file not found')
-        
-    ml_input = pd.DataFrame.from_dict({param.name: [param.value] for param in pre_in.parameters})
-    LOGGER.debug('ML input: %s', ml_input)
-    
-    ml_output = deserialized.predict({'instances': ml_input})
-    LOGGER.debug('ML output: %s', ml_output)
+            status_code=status.HTTP_404_NOT_FOUND, detail='Resource not found')
 
-    try:
-        prediction_output = {'result': {**{k: v[0] for k, v in ml_output.items()}}}
-    except KeyError:
-        prediction_output = {'result': ml_output}
-    LOGGER.info('Prediction output : %s', prediction_output)
-    # return flatmap
-    return prediction_output
+    params = [param for param in pre_in.parameters]
+    prediction_output = deserialized.predict(params)
+    score_output = deserialized.predict_proba(params)
+
+    LOGGER.info('Prediction output: %s, Score output: %s', prediction_output, score_output)
+    if score_output is not None:
+        return {'result': {'predictions': prediction_output, 'scores': score_output}}
+    else:
+        return {'result': {'predictions': prediction_output}}
