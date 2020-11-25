0512f7fa62d4413926e5c89f782864ca1be6b49a
Marouane Boudhaim <Marouane.Boudhaim@ibm.com>
2020-11-25 13:37:54 +0100

    Add support for multiple scikit models

--- ml-service-implementations/sklearn-service/openapi_server/controllers/helper.py
+++ ml-service-implementations/sklearn-service/openapi_server/controllers/helper.py
@@ -1,6 +1,9 @@
 import json
+import os
 
-supported_models=["regression"]
+
+
+supported_models=[ root[7:] for root, dir, files in os.walk("./data") for file in files if file.endswith("model.pkl")]
 
 def get_model_conf(model_id):
     return json.load(open(f"data/{model_id}/deployment_conf.json","r"))
