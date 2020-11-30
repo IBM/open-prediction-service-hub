9d6b0fbce6085db8eece845f8c9f34f65f4b51fa
ke li <Ke.Li1@ibm.com>
2020-11-30 15:22:45 +0100

    Update ORM for new model_config

--- ml-service-implementations/ads-ml-service/app/crud/crud_model_config.py
+++ ml-service-implementations/ads-ml-service/app/crud/crud_model_config.py
@@ -15,32 +15,55 @@
 #
 
 
+import datetime
 import typing
 
 import fastapi.encoders as encoders
 import sqlalchemy.orm as orm
 
+import app.crud.base as app_crud_base
 import app.models as models
 import app.schemas as schemas
-from .base import CRUDBase, IdType
 
 
-class CRUDModelConfig(CRUDBase[models.ModelConfig, schemas.ModelConfigCreate, schemas.ModelConfigUpdate]):
+class CRUDModelConfig(app_crud_base.CRUDBase[models.ModelConfig, schemas.ModelConfigCreate, schemas.ModelConfigUpdate]):
     def create_with_model(
-            self, db: orm.Session, *, obj_in: schemas.ModelConfigCreate, model_id: IdType
+            self, db: orm.Session, *, obj_in: schemas.ModelConfigCreate, model_id: app_crud_base.IdType
     ) -> models.ModelConfig:
         # noinspection PyArgumentList
         db_obj = self.model(
+            id=model_id,
             **encoders.jsonable_encoder(obj_in),
-            model_id=model_id
         )
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
         return db_obj
 
-    def get_by_model(self, db: orm.Session, *, model_id: IdType) -> typing.Optional[models.ModelConfig]:
-        return db.query(self.model).filter(self.model.model_id == model_id).first()
+    def get_by_model(self, db: orm.Session, *, model_id: app_crud_base.IdType) -> typing.Optional[models.ModelConfig]:
+        return self.get(db, id=model_id)
+
+    def update(
+            self,
+            db: orm.Session,
+            *,
+            db_obj: models.ModelConfig,
+            obj_in: typing.Union[schemas.ModelConfigUpdate, typing.Dict[typing.Text, typing.Any]]
+    ) -> models.ModelConfig:
+        model = db.query(models.Model).filter(models.Model.id == db_obj.id).first()
+        model.modified_at = datetime.datetime.now(tz=datetime.timezone.utc)
+
+        original = encoders.jsonable_encoder(db_obj)
+        update_data = obj_in if isinstance(obj_in, typing.Dict) else obj_in.dict(exclude_unset=True)
+        for field in original:
+            if field in update_data:
+                setattr(db_obj, field, update_data[field])
+
+        db.add(model)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
 
 
 model_config = CRUDModelConfig(models.ModelConfig)
