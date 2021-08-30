d3d5d36b46c2962ecf06a1d72287d8095d3e0f44
ke li <Ke.Li1@ibm.com>
2021-08-30 14:59:40 +0200

    [ADSML-200] Refactor prestart.sh

--- ops-implementations/ads-ml-service/prestart.sh
+++ ops-implementations/ads-ml-service/prestart.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Make sure DB is started
-python3 "${OPS_HOME}"/app/backend_pre_start.py
+python3 "${__dir}"/app/backend_pre_start.py
 
 # Init DB
-python3 "${OPS_HOME}"/app/initial_data.py
+python3 "${__dir}"/app/initial_data.py
