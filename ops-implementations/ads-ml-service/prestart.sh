cf8001194a089d854cf6f427bbf9ea04049f73e1
ke li <Ke.Li1@ibm.com>
2021-10-15 08:59:08 +0200

    [ADSML-225] Add alembic

--- ops-implementations/ads-ml-service/prestart.sh
+++ ops-implementations/ads-ml-service/prestart.sh
@@ -9,5 +9,8 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Make sure DB is started
 python3 "${__dir}"/app/backend_pre_start.py
 
+# Run migrations
+python3 -m alembic upgrade head
+
 # Init DB
 python3 "${__dir}"/app/initial_data.py
