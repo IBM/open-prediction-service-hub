c80005085d8f8a892d8c8041f8166d4b4177e20c
ke li <Ke.Li1@ibm.com>
2022-01-10 17:26:39 +0100

    [ADSML-285] Rename swagger to openapi

---
+++ ops-implementations/sagemaker-service/openapi_server/models/__init__.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from openapi_server.models.any_of_parameter_value import AnyOfParameterValue
+from openapi_server.models.capabilities import Capabilities
+from openapi_server.models.capability import Capability
+from openapi_server.models.endpoint import Endpoint
+from openapi_server.models.endpoint_creation import EndpointCreation
+from openapi_server.models.endpoint_update import EndpointUpdate
+from openapi_server.models.endpoints import Endpoints
+from openapi_server.models.error import Error
+from openapi_server.models.feature import Feature
+from openapi_server.models.input_schema import InputSchema
+from openapi_server.models.link import Link
+from openapi_server.models.model import Model
+from openapi_server.models.model_creation import ModelCreation
+from openapi_server.models.model_update import ModelUpdate
+from openapi_server.models.model_update_metadata import ModelUpdateMetadata
+from openapi_server.models.models import Models
+from openapi_server.models.output_schema import OutputSchema
+from openapi_server.models.parameter import Parameter
+from openapi_server.models.prediction import Prediction
+from openapi_server.models.prediction_response import PredictionResponse
+from openapi_server.models.server_info import ServerInfo
