c3b779851a452e539657b1cb4d13db578252fd17
ke li <Ke.Li1@ibm.com>
2022-07-12 09:22:30 +0200

    [DBACLD-55843] Add debug launcher

--- ops-implementations/ads-ml-service/app/main.py
+++ ops-implementations/ads-ml-service/app/main.py
@@ -16,17 +16,20 @@
 
 
 import logging.config
+import os
+import pathlib
+import tempfile
 
 import fastapi
 import fastapi.responses as responses
 import yaml
 
-import app.api.api_v2.api as api_v2
-import app.core.configuration as app_conf
-import app.version as ads_ml_service_version
-
 
 def get_app() -> fastapi.FastAPI:
+    import app.api.api_v2.api as api_v2
+    import app.core.configuration as app_conf
+    import app.version as ads_ml_service_version
+
     app = fastapi.FastAPI(
         version=ads_ml_service_version.__version__,
         title='ADS ML Service',
@@ -47,3 +50,32 @@ def get_app() -> fastapi.FastAPI:
 
     app.include_router(api_v2.api_router)
     return app
+
+
+def launch_test_server():
+    import asyncio
+
+    import hypercorn.config as h_config
+    import hypercorn.asyncio as h_asyncio
+
+    with tempfile.TemporaryDirectory() as tmp:
+        root = pathlib.Path(tmp)
+        current = pathlib.Path(__file__).parent
+
+        logging_configs = yaml.safe_load((current / '..' / 'logging.yaml').read_text())
+        logging_configs['handlers']['info_file_handler']['filename'] = str(root / 'info.log')
+        logging_configs['handlers']['error_file_handler']['filename'] = str(root / 'error.log')
+        os.environ['LOGGING'] = str(root / 'logging.yaml')
+        os.environ['DEBUG'] = str(True)
+        os.environ['SETTINGS'] = str(root)
+        os.environ['MODEL_STORAGE'] = str(root)
+        (root / 'logging.yaml').write_text(yaml.dump(logging_configs))
+
+        app = get_app()
+        hypercorn_config = h_config.Config()
+        hypercorn_config.bind = ["localhost:8080"]
+        asyncio.run(h_asyncio.serve(app, hypercorn_config))
+
+
+if __name__ == '__main__':
+    launch_test_server()
